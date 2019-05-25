9af81e09ca7674520ae7c65dadb9af11c1ff68b6
Ravan Scafi <rscafi@gmail.com>
2019-05-25 00:05:42 -0300

    Use bigger layout max-width

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 1032,
               showCaptions: true,
               quality: 100,
               withWebp: true,
