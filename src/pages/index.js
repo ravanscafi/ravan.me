f367e847edf20b92c2d87516729ff95cff97f5c8
Ravan Scafi <rscafi@gmail.com>
2019-05-30 22:11:27 -0300

    Change home site title (#20)

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="Home" />
         <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
