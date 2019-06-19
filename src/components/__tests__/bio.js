a7d70911f6469e741758ce3ce7b82493b375db3e
Ravan Scafi <rscafi@gmail.com>
2019-06-19 00:18:32 -0300

    Create snapshot tests for components

---
+++ src/components/__tests__/bio.js
@@ -0,0 +1,44 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import {StaticQuery} from "gatsby"
+
+import Bio from "../bio"
+
+beforeEach(() => {
+  StaticQuery.mockImplementationOnce(({render}) =>
+    render({
+        "avatar": {
+          "childImageSharp": {
+            "fixed": {
+              "base64": "data:image/jpeg;base64,/9j/SHAcv//Z",
+              "width": 150,
+              "height": 150,
+              "src": "/static/src/profile-pic.jpg",
+              "srcSet": "/static/srcset/profile-pic.jpg",
+            }
+          }
+        },
+        "site": {
+          "siteMetadata": {
+            "author": "Ravan Scafi",
+            "social": {
+              "twitter": "ravanscafi",
+              "github": "ravanscafi",
+              "linkedin": "ravanscafi",
+              "stackoverflow": "804741/ravan-scafi"
+            }
+          }
+        }
+      }
+    )
+  )
+})
+
+describe("Bio", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Bio><h1>John Doe</h1></Bio>)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
---
+++ src/components/icons/__tests__/stackoverflow.js
@@ -0,0 +1,13 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Stackoverflow from "../stackoverflow"
+
+describe("Stackoverflow", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Stackoverflow />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
