3d5c7813458cdfdeb22ee25d1f5bf1f6a52199aa
Ravan Scafi <rscafi@gmail.com>
2019-07-11 23:54:14 -0300

    Remove drafts from tag template

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -41,7 +41,10 @@ export const pageQuery = graphql`
     allMarkdownRemark(
       limit: 1000
       sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { tags: { in: [$tag] } } }
+      filter: {
+        frontmatter: { tags: { in: [$tag] } }
+        fields: { draft: { eq: false } }
+      }
     ) {
       totalCount
       edges {
