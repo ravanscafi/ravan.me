12a9394b7c83d585890a3c1ccab5b86e9e62d2d0
Ravan Scafi <rscafi@gmail.com>
2019-07-13 23:16:51 -0300

    Refactor blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Disqus from "gatsby-plugin-disqus"
 import Calendar from "react-feather/dist/icons/calendar"
 import Clock from "react-feather/dist/icons/clock"
@@ -10,84 +10,87 @@ import SEO from "../components/seo"
 import TagList from "../components/tag-list"
 import { rhythm, scale } from "../utils/typography"
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const site = this.props.data.site
-    const siteTitle = site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
-    const { title, description, cover, date, tags } = post.frontmatter
+const BlogPostTemplate = ({
+  data: {
+    markdownRemark: {
+      id,
+      excerpt,
+      fields: { readingTime, slug },
+      frontmatter: { cover, date, description, tags, title },
+      html,
+    },
+    site: {
+      siteMetadata: { author, siteUrl, title: siteTitle },
+    },
+  },
+  pageContext: { previous, next },
+  location,
+}) => {
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={title}
+        description={description || excerpt}
+        cover={cover.childImageSharp.fixed.src}
+      />
+      <h1>{title}</h1>
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: `block`,
+          marginBottom: rhythm(1),
+          marginTop: rhythm(-1),
+        }}
+      >
+        <Calendar className="icon" /> {date}
+        <span style={{ padding: `0 .5rem` }}>&bull;</span>
+        <span>
+          <Clock className="icon" /> {readingTime.text}
+        </span>
+        <span style={{ padding: `0 .5rem` }}>&bull;</span>
+        <span>
+          <TagList tags={tags} />
+        </span>
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <hr
+        style={{
+          marginBottom: rhythm(1),
+        }}
+      />
+      <Bio>
+        <div>
+          <strong>{author}</strong>
+        </div>
+      </Bio>
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={title}
-          description={description || post.excerpt}
-          cover={cover.childImageSharp.fixed.src}
-        />
-        <h1>{title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          <Calendar className="icon" /> {date}
-          <span style={{ padding: `0 .5rem` }}>&bull;</span>
-          <span>
-            <Clock className="icon" /> {post.fields.readingTime.text}
-          </span>
-          <span style={{ padding: `0 .5rem` }}>&bull;</span>
-          <span>
-            <TagList tags={tags} />
-          </span>
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio>
-          <div>
-            <strong>{site.siteMetadata.author}</strong>
-          </div>
-        </Bio>
-
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-        <Disqus
-          identifier={post.id}
-          title={title}
-          url={`${site.siteMetadata.siteUrl}${post.fields.slug}`}
-        />
-      </Layout>
-    )
-  }
+      <ul
+        style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+          justifyContent: `space-between`,
+          listStyle: `none`,
+          padding: 0,
+        }}
+      >
+        <li>
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </li>
+      </ul>
+      <Disqus identifier={id} title={title} url={`${siteUrl}${slug}`} />
+    </Layout>
+  )
 }
 
 export default BlogPostTemplate
