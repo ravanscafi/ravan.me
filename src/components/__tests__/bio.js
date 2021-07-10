7c0865475001e3e613da06c2cb0971838548115d
Ravan Scafi <rscafi@gmail.com>
2021-07-10 20:27:34 -0300

    Fix tests

--- src/components/__tests__/bio.js
+++ src/components/__tests__/bio.js
@@ -1,6 +1,6 @@
 import React from "react"
 import renderer from "react-test-renderer"
-import { useStaticQuery } from "gatsby"
+import {useStaticQuery} from "gatsby"
 
 import Bio from "../bio"
 
@@ -8,12 +8,25 @@ beforeEach(() => {
   useStaticQuery.mockImplementationOnce(() => ({
     avatar: {
       childImageSharp: {
-        fixed: {
-          base64: "data:image/jpeg;base64,/9j/SHAcv//Z",
-          width: 150,
-          height: 150,
-          src: "/static/src/profile-pic.jpg",
-          srcSet: "/static/srcset/profile-pic.jpg",
+        "gatsbyImageData": {
+          "layout": "constrained",
+          "backgroundColor": "#c8c8d8",
+          "images": {
+            "fallback": {
+              "src": "/static/353dd4cfe9bc5723e98b15bf2d130c9b/4a43d/profile-pic.jpg",
+              "srcSet": "/static/353dd4cfe9bc5723e98b15bf2d130c9b/b7413/profile-pic.jpg 45w,\n/static/353dd4cfe9bc5723e98b15bf2d130c9b/a9a36/profile-pic.jpg 90w,\n/static/353dd4cfe9bc5723e98b15bf2d130c9b/4a43d/profile-pic.jpg 180w,\n/static/353dd4cfe9bc5723e98b15bf2d130c9b/90089/profile-pic.jpg 360w",
+              "sizes": "(min-width: 180px) 180px, 100vw"
+            },
+            "sources": [
+              {
+                "srcSet": "/static/353dd4cfe9bc5723e98b15bf2d130c9b/29677/profile-pic.webp 45w,\n/static/353dd4cfe9bc5723e98b15bf2d130c9b/5d191/profile-pic.webp 90w,\n/static/353dd4cfe9bc5723e98b15bf2d130c9b/52f83/profile-pic.webp 180w,\n/static/353dd4cfe9bc5723e98b15bf2d130c9b/27ab5/profile-pic.webp 360w",
+                "type": "image/webp",
+                "sizes": "(min-width: 180px) 180px, 100vw"
+              }
+            ]
+          },
+          "width": 180,
+          "height": 180
         },
       },
     },
