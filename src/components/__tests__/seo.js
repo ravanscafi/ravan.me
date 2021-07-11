15e5e8fac90892644b863fb6abe411c35679c0ca
Ravan Scafi <rscafi@gmail.com>
2021-07-11 20:20:05 -0300

    Fix tests

--- src/components/__tests__/seo.js
+++ src/components/__tests__/seo.js
@@ -1,6 +1,6 @@
 import React from "react"
 import renderer from "react-test-renderer"
-import {useStaticQuery} from "gatsby"
+import { useStaticQuery } from "gatsby"
 
 import Seo from "../seo"
 
@@ -16,26 +16,28 @@ beforeEach(() => {
     },
     image: {
       childImageSharp: {
-        "gatsbyImageData": {
-          "layout": "constrained",
-          "backgroundColor": "#383838",
-          "images": {
-            "fallback": {
-              "src": "/static/a379ede7190febde66f2453a8c9ac9c1/257af/icon.png",
-              "srcSet": "/static/a379ede7190febde66f2453a8c9ac9c1/25ed1/icon.png 90w,\n/static/a379ede7190febde66f2453a8c9ac9c1/a2c25/icon.png 180w,\n/static/a379ede7190febde66f2453a8c9ac9c1/257af/icon.png 360w,\n/static/a379ede7190febde66f2453a8c9ac9c1/6c9d4/icon.png 720w",
-              "sizes": "(min-width: 360px) 360px, 100vw"
+        gatsbyImageData: {
+          layout: "constrained",
+          backgroundColor: "#383838",
+          images: {
+            fallback: {
+              src: "/static/a379ede7190febde66f2453a8c9ac9c1/257af/icon.png",
+              srcSet:
+                "/static/a379ede7190febde66f2453a8c9ac9c1/25ed1/icon.png 90w,\n/static/a379ede7190febde66f2453a8c9ac9c1/a2c25/icon.png 180w,\n/static/a379ede7190febde66f2453a8c9ac9c1/257af/icon.png 360w,\n/static/a379ede7190febde66f2453a8c9ac9c1/6c9d4/icon.png 720w",
+              sizes: "(min-width: 360px) 360px, 100vw",
             },
-            "sources": [
+            sources: [
               {
-                "srcSet": "/static/a379ede7190febde66f2453a8c9ac9c1/5d191/icon.webp 90w,\n/static/a379ede7190febde66f2453a8c9ac9c1/52f83/icon.webp 180w,\n/static/a379ede7190febde66f2453a8c9ac9c1/27ab5/icon.webp 360w,\n/static/a379ede7190febde66f2453a8c9ac9c1/88c6b/icon.webp 720w",
-                "type": "image/webp",
-                "sizes": "(min-width: 360px) 360px, 100vw"
-              }
-            ]
+                srcSet:
+                  "/static/a379ede7190febde66f2453a8c9ac9c1/5d191/icon.webp 90w,\n/static/a379ede7190febde66f2453a8c9ac9c1/52f83/icon.webp 180w,\n/static/a379ede7190febde66f2453a8c9ac9c1/27ab5/icon.webp 360w,\n/static/a379ede7190febde66f2453a8c9ac9c1/88c6b/icon.webp 720w",
+                type: "image/webp",
+                sizes: "(min-width: 360px) 360px, 100vw",
+              },
+            ],
           },
-          "width": 360,
-          "height": 360
-        }
+          width: 360,
+          height: 360,
+        },
       },
     },
   }))
