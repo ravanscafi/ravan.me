10d8b1b120845bd12d5412746e6e03b07baed384
Ravan Scafi <rscafi@gmail.com>
2022-02-21 10:34:09 +0100

    Update dependencies

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
   },
   moduleNameMapper: {
+    "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
